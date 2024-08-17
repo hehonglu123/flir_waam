1722a4f5babc66015677cff30d7dffd623db812b
hehonglu123 <hehonglu123@126.com>
2024-08-16 23:54:31 -0400

    updates

--- yolov8/data_gen_wire.py
+++ yolov8/data_gen_wire.py
@@ -6,16 +6,20 @@ import matplotlib.pyplot as plt
 
 
 data_dir='../../../recorded_data/'
-data_sets=['ER316L/wallbf_70ipm_v7_70ipm_v7/','ER316L/wallbf_150ipm_v15_150ipm_v15/',
-           'ER316L/trianglebf_100ipm_v10_100ipm_v10','ER316L/cylinderspiral_100ipm_v10/',
-           'ER316L/streaming/cylinderspiral_T19000/','ER316L/streaming/cylinderspiral_T25000/',
-           'ER316L/VPD10/cylinderspiral_50ipm_v5','ER316L/VPD10/cylinderspiral_70ipm_v7',
-           'ER316L/VPD10/cylinderspiral_110ipm_v11','ER316L/VPD10/cylinderspiral_130ipm_v13']
+# data_sets=['ER316L/wallbf_70ipm_v7_70ipm_v7/','ER316L/wallbf_150ipm_v15_150ipm_v15/',
+#            'ER316L/trianglebf_100ipm_v10_100ipm_v10','ER316L/cylinderspiral_100ipm_v10/',
+#            'ER316L/streaming/cylinderspiral_T19000/','ER316L/streaming/cylinderspiral_T25000/',
+#            'ER316L/VPD10/cylinderspiral_50ipm_v5','ER316L/VPD10/cylinderspiral_70ipm_v7',
+#            'ER316L/VPD10/cylinderspiral_110ipm_v11','ER316L/VPD10/cylinderspiral_130ipm_v13']
+
+# data_sets=['ER316L/VPD10/tubespiral_70ipm_v7','ER316L/VPD10/tubespiral_150ipm_v15']
+# data_sets=['ER316L/VPD10/tubespiral_50ipm_v5']
+data_sets=['ER316L/VPD10/tubespiral_180ipm_v18']
 
 output_dir='../../../recorded_data/yolov8/tip-wire/'
 
 num_images_per_set=10
-img_counts=0
+img_counts=130
 #radmonly select 10 images from each set
 for data_set in data_sets:
     with open(data_dir+data_set+'/ir_recording.pickle', 'rb') as file:
